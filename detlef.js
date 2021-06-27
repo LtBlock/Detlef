80e6d5fa1f6dcc115da9cafd5d44e85f454b4baa
A.Schatz <schatz.as@t-online.de>
2021-06-27 09:48:50 +0200

    Init
    Detlef can react to !react and to !poke

---
+++ detlef.js
@@ -0,0 +1,27 @@
+const {token, prefix} = require('./config.json');
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Hello There!');
+})
+
+client.on('message', msg => {
+    if(!msg.content.startsWith(prefix) || msg.author.bot) return;
+    
+    // split test message into command and arguments => !poke me = command 'poke' with args 'me'
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'poke') {
+        msg.channel.send('FASS MICH NICHT AN!');
+        if(args.length) msg.channel.send(`"${args}" \nverstehe ich nicht.`);
+    } else if(command === 'react') {
+        msg.react(getEmoji(msg.guild.emojis, "Uff"));
+    }
+})
+
+client.login(token);
+
+const getEmoji = (emojis, name) =>(emojis.cache.find(emoji => emoji.name === name));
